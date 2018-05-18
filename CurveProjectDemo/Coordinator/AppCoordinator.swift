94d251f87ea0ec5160d5b1fe5827cefa37b190da
Sam Watts <samuel.watts@gmail.com>
2018-05-18 15:15:44 +0100

    added an app coordinator to create the initial view controller

---
+++ CurveProjectDemo/Coordinator/AppCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  AppCoordinator.swift
+//  CurveProjectDemo
+//
+//  Created by Sam Watts on 18/05/2018.
+//  Copyright © 2018 Sam Watts. All rights reserved.
+//
+
+import UIKit
+
+final class AppCoordinator {
+
+    private let window: UIWindow
+
+    init(window: UIWindow = UIWindow(frame: UIScreen.main.bounds)) {
+        self.window = window
+    }
+
+    func start() {
+        let cardsAndTransactionViewController = UIViewController()
+        let rootNavigationController = UINavigationController(rootViewController: cardsAndTransactionViewController)
+
+        window.rootViewController = rootNavigationController
+
+        window.makeKeyAndVisible()
+    }
+}
